790744c272dd688c3c202ae1a0660ed62af412c3
Stanislav Fishkov <fishkovs@yandex.ru>
2025-02-22 20:30:38 +0100

    Add discovery-server module

--- stat/stat-client/src/main/java/ru/practicum/stats/client/StatsClientImpl.java
+++ stat/stat-client/src/main/java/ru/practicum/stats/client/StatsClientImpl.java
@@ -4,18 +4,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriComponentsBuilder;
+import ru.practicum.stats.client.error.exception.StatsServerUnavailable;
 import ru.practicum.stats.dto.HitDto;
 import ru.practicum.stats.dto.StatsDto;
 import ru.practicum.stats.dto.StatsRequestParamsDto;
 import ru.practicum.stats.utils.DateTimeUtil;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,20 +29,37 @@ import java.util.Objects;
 @Component
 public class StatsClientImpl implements StatClient {
     private final RestTemplate rest;
+    private final DiscoveryClient discoveryClient;
+    private final String statsServiceId;
+    private final RetryTemplate retryTemplate;
 
     @Autowired
-    public StatsClientImpl(@Value("${stats-server.url}") String statUrl, RestTemplateBuilder builder) {
+    public StatsClientImpl(@Value("${stats-server.id}") String statsServiceId, RestTemplateBuilder builder,
+                           DiscoveryClient discoveryClient) {
+        this.statsServiceId = statsServiceId;
+
         this.rest = builder
-                .uriTemplateHandler(new DefaultUriBuilderFactory(statUrl))
                 .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
                 .build();
+
+        this.discoveryClient = discoveryClient;
+
+        retryTemplate = new RetryTemplate();
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(3000L);
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy();
+        retryPolicy.setMaxAttempts(3);
+        retryTemplate.setRetryPolicy(retryPolicy);
     }
 
     @Override
     public void hit(HitDto hitDto) {
         HttpEntity<HitDto> requestEntity = new HttpEntity<>(hitDto, defaultHeaders());
         try {
-            rest.exchange("/hit", HttpMethod.POST, requestEntity, Object.class);
+            rest.exchange(makeUri("/hit"), HttpMethod.POST, requestEntity, Object.class);
         } catch (HttpStatusCodeException e) {
             log.error("Hit stats was not successful with code {} and message {}", e.getStatusCode(), e.getMessage(), e);
         } catch (Exception e) {
@@ -65,10 +88,11 @@ public class StatsClientImpl implements StatClient {
                 .encode()
                 .toUriString();
 
+
         HttpEntity<String> requestEntity = new HttpEntity<>(defaultHeaders());
         ResponseEntity<StatsDto[]> statServerResponse;
         try {
-            statServerResponse = rest.exchange(uri, HttpMethod.GET, requestEntity, StatsDto[].class);
+            statServerResponse = rest.exchange(makeUri(uri), HttpMethod.GET, requestEntity, StatsDto[].class);
         } catch (HttpStatusCodeException e) {
             log.error("Get stats was not successful with code {} and message {}", e.getStatusCode(), e.getMessage(), e);
             return List.of();
@@ -98,5 +122,23 @@ public class StatsClientImpl implements StatClient {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         return headers;
+    }
+
+    private ServiceInstance getInstance() {
+        try {
+            return discoveryClient
+                    .getInstances(statsServiceId)
+                    .getFirst();
+        } catch (Exception exception) {
+            throw new StatsServerUnavailable(
+                    "Stats server's not been discovered with id: " + statsServiceId,
+                    exception
+            );
+        }
+    }
+
+    private URI makeUri(String path) {
+        ServiceInstance instance = retryTemplate.execute(cxt -> getInstance());
+        return URI.create("http://" + instance.getHost() + ":" + instance.getPort() + path);
     }
 }
